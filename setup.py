2853fb07e416fd766ac13e34dedb6b949342c5e2
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2023-06-05 15:22:35 +0200

    added version in setup

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 """Setup file for prototype template."""
 
 import setuptools
+import os
 
 with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
@@ -8,9 +9,15 @@ with open("README.md", encoding="utf-8") as f:
 with open("requirements.txt") as f:
     install_requires = f.read().splitlines()
 
+here = os.path.abspath(os.path.dirname(__file__))
+version = {}
+with open(os.path.join(here, "vqls_prototype", "__version__.py")) as f:
+    exec(f.read(), version)
+
 setuptools.setup(
     name="vqls_prototype",
-    description="Repository for a quantum prototype",
+    version=version["__version__"],
+    description="Prototype for a Quantum Linear Solver",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
