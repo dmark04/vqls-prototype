db073b6453af0fad1fbd20762af549e96421c454
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2023-11-01 20:14:27 +0100

    Update __init__.py
    change __init__

--- vqls_prototype/__init__.py
+++ vqls_prototype/__init__.py
@@ -15,11 +15,21 @@
 Variational Quantum Linear Solver
 =======================================
 """
+"""Template project."""
 
+from importlib_metadata import version as metadata_version, PackageNotFoundError
+
+from .template_module import TemplateClass
 from .solver.vqls import VQLS
 from .solver.log import VQLSLog
 from .solver.hybrid_qst_vqls import Hybrid_QST_VQLS
 from .solver.qst_vqls import QST_VQLS
 
+
+try:
+    __version__ = metadata_version("prototype_template")
+except PackageNotFoundError:  # pragma: no cover
+    # package is not installed
+    pass
 
 __all__ = ["VQLS", "VQLSLog", "Hybrid_QST_VQLS", "QST_VQLS"]
