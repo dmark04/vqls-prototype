e111d7459d130db73f0536c2439b62803ac7f1c0
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2023-11-01 20:38:54 +0100

    Update __init__.py

--- vqls_prototype/__init__.py
+++ vqls_prototype/__init__.py
@@ -1,24 +1,6 @@
-# This code is part of Qiskit.
-#
-# (C) Copyright IBM 2022.
-#
-# This code is licensed under the Apache License, Version 2.0. You may
-# obtain a copy of this license in the LICENSE.txt file in the root directory
-# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
-#
-# Any modifications or derivative works of this code must retain this
-# copyright notice, and modified files need to carry a notice indicating
-# that they have been altered from the originals.
-
-"""
-=======================================
-Variational Quantum Linear Solver
-=======================================
-"""
-"""Template project."""
+"""VQLS"""
 
 from importlib_metadata import version as metadata_version, PackageNotFoundError
-
 
 from .solver.vqls import VQLS
 from .solver.log import VQLSLog
