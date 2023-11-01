7986f756c51e4998fd5ee37a2064c3a32972bd50
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2023-11-01 20:09:21 +0100

    Update __init__.py
    update __init__ to deal with the metadata

--- vqls_prototype/__init__.py
+++ vqls_prototype/__init__.py
@@ -16,9 +16,22 @@ Variational Quantum Linear Solver
 =======================================
 """
 
+"""Template project."""
+
+from importlib_metadata import version as metadata_version, PackageNotFoundError
+
+from .template_module import TemplateClass
+
 from .vqls import VQLS, VQLSLog
 from .hadamard_test import HadammardTest, HadammardOverlapTest
 from .matrix_decomposition import SymmetricDecomposition, PauliDecomposition
+
+
+try:
+    __version__ = metadata_version("vqls_template")
+except PackageNotFoundError:  # pragma: no cover
+    # package is not installed
+    pass
 
 __all__ = [
     "VQLS",
